93b1bda85d00388e517eb59dcec194e310d11545
Trevor Lalish-Menagh <trev@trevreport.org>
2011-03-19 13:00:05 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaalWebApp::Application.config.secret_token = '1ffd4cf08b327d4dd7eaca951be116adea5257419763f1eebf45ab997ae28f136ccaf0448a095c840ddb849ce3bf6ec42b6e97d2525be5f1dbfbec0bcb2e5c9e'
