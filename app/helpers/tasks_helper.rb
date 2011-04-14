cf86d7ba4d2734a48635477d31321922a40c2259
Trevor Lalish-Menagh <trev@trevreport.org>
2011-04-13 20:56:57 -0400

    Fixed overflow for images and text in components; Added typing to tasks in new and edit forms

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,8 @@
 module TasksHelper
+  def submit_default_value(task)
+    key = task ? (task.persisted? ? :update : :create) : :submit
+    task_type = (task && task.task_type) ? task.task_type.capitalize : I18n.t(:task)
+
+    I18n.t(:"helpers.submit.task.#{key}", :task_type => task_type)
+  end
 end
